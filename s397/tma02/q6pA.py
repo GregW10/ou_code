b4db83d1f36148a50948c30140204617b4b59a06
Gregor Hartl Watters <mario.garwh@gmail.com>
2025-02-17 22:56:53 +0100

    Added a small script.

---
+++ s397/tma02/q6pA.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def main():
+    # ###############
+    print("Question a")
+    GPP = 1_400 # gC m^-2 yr^-1
+    NPP =   600 # gC m^-2 yr^-1
+    R_a = GPP - NPP
+    print(f"Autotrophic respiration: {R_a} gC m^-2 yr^-1")
+    # ###############
+    print("\nQuestion b")
+
+
+if __name__ == "__main__":
+    main()
+
